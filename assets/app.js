0a3698c5c0fbb0ed0c8dfbc4cd675db6fca7c696
Shrey <shrey.vakil@mail.utoronto.ca>
2021-05-16 15:53:09 -0400

    Desktop version almost done

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,15 @@
 (function() {
 	"use strict";
-
-	// Add Scripts Here
-    	
+	var x = document.getElementById("hidden-container");
+	x.style.display = "none";
+	// Add Scripts Here 
 })();
+
+function toggle() {
+	var x = document.getElementById("hidden-container");
+	if (x.style.display === "none") {
+	  x.style.display = "block";
+	} else {
+	  x.style.display = "none";
+	}
+  }
