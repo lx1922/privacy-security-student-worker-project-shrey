2bc7ac475a3479adb7086de5f1def18f355b9b4e
Shrey <shrey.vakil@mail.utoronto.ca>
2021-05-18 20:27:01 -0400

    Incomplete Submission

--- assets/app.js
+++ assets/app.js
@@ -7,9 +7,12 @@
 
 function toggle() {
 	var x = document.getElementById("hidden-container");
+	var y = document.getElementById("show_details");
 	if (x.style.display === "none") {
 	  x.style.display = "block";
+	  y.innerHTML = "Hide Details";
 	} else {
 	  x.style.display = "none";
+	  y.innerHTML = "Show Details";
 	}
   }
